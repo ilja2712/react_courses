1a140a27bf788715d3b542ed64727671f01e6dc9
Ilja <kozlikov.ilja@yandex.ru>
2021-11-29 17:40:39 +0300

    React JSX

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const Book = (props) => {
+  return React.createElement('div', {}, [
+      React.createElement('h2', {}, props.name),
+      React.createElement('p', {}, props.year),
+      React.createElement('p', {}, props.price),
+  ]);
+}
+
+
+const App = () => {
+  return (
+      <div>
+          <Book name="Javascript" year="2018" price="1200" />
+          <Book name="Angular" year="2019" price="1000" />
+          <Book name="React for beginners" year="2021" price="900" />
+      </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <App />    
+  </React.StrictMode>, 
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
