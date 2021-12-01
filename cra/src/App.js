b85c1451e5325d66183f18e5e8b51ecbd07e4b53
Ilja <kozlikov.ilja@yandex.ru>
2021-12-01 16:27:09 +0300

    App React with component classes

---
+++ cra/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        Hello from React!
+      </div>
+    );
+  }
+}
+
+export default App;
