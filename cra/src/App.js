7c995cea06f7285eee27214ad7002d0687f8d6ff
Ilja <kozlikov.ilja@yandex.ru>
2021-12-06 15:28:42 +0300

    Timer React

--- cra/src/App.js
+++ cra/src/App.js
@@ -6,39 +6,56 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [],
-      isLoading: true,
-      comments: []
+      count: 0,
+      isStart: false
     }
+
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
   }
+
+
 
   componentDidMount() {
     console.log('componentDidMount');
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => this.setState({posts: data, isLoading: false}))
-
-      this.timerId = setInterval(() => {
-        fetch('https://jsonplaceholder.typicode.com/comments')
-          .then(response => response.json())
-          .then(data => this.setState({comments: data, isLoading: false}))
-      }, 3000)
+    if (localStorage.getItem('timer')) {
+      this.setState({count: Number(localStorage.getItem('timer'))});
+    }      
   }
 
   componentDidUpdate() {
     console.log('componentDidUpdate');
+    localStorage.setItem('timer', this.state.count);
   }
 
   componentWillUnmount() {
     console.log('componentWillUnmount');
+  }
+
+  stopTimer() {
+    this.setState({isStart: false});
     clearInterval(this.timerId);
+  }
+
+  startTimer() {
+    this.timerId = setInterval(() => {
+      this.setState({count: this.state.count + 1});
+    }, 1000);
+    this.setState({isStart: true});
+  }
+
+  resetTimer() {
+    this.setState({count: 0});
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.isLoading ? <h3>Loading...</h3> : <h3>{this.state.posts.length} posts load</h3>}
-        {this.state.isLoading ? <h4>Loading...</h4> : <h4>{this.state.comments.length} comments load</h4>}
+        <span>{this.state.count}</span>
+        <br />
+        {this.state.isStart ? <button onClick={this.stopTimer}>Stop</button> : <button onClick={this.startTimer}>Start</button>}
+        <button onClick={this.resetTimer}>Reset</button>
       </div>
     );
   }
